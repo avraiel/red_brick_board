da4ed91f85b458cff10970830666c39b8911ceb0
gab <gabriel.geraldo@student.ateneo.edu>
2024-03-12 01:09:21 +0800

    created CustomUserManager for easier create super/user

--- redbrickboard/accounts/models.py
+++ redbrickboard/accounts/models.py
@@ -1,5 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email for user must be set.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -18,6 +39,8 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, max_length=150)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.first_name
