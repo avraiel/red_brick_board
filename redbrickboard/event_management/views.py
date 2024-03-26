98aa801e0a556d6d1a916e59b6388aaf2c836721
Ciella <daniella.francisco@student.ateneo.edu>
2024-03-26 21:14:37 +0800

    Updated Event Management views and URLs

--- redbrickboard/event_management/views.py
+++ redbrickboard/event_management/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 
-def index(request):
-    return HttpResponse('Hello World! This came from the index view.')
+from .models import Event
+
+def event_view(request):
+    events = Event.objects.all()
+    context = {'events' : events}
+    return render(request, 'event_management/events.html', context)
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'event_management/event-details.html'
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'event_management/event-list.html'
+
+class EventCreateView(CreateView):
+    model = Event
+    fields = '__all__'
+    template_name = 'event_management/event-create.html'
+
+class EventUpdateView(UpdateView):
+    model = Event
+    fields = '__all__'
+    template_name = 'event/event-update.html'
