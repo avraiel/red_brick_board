f5f1c65fd2b8e46d624c2b74933eabe85deeb908
Ciella <daniella.francisco@student.ateneo.edu>
2024-04-12 05:58:07 +0800

    Modified the URLs by removing the FBV class and replacing the event homepage to the list view of the events

--- redbrickboard/event_management/urls.py
+++ redbrickboard/event_management/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import event_view, EventListView, EventDetailView, EventCreateView, EventUpdateView
+from .views import EventListView, EventDetailView, EventCreateView, EventUpdateView
 
 urlpatterns = [
-    path('', event_view, name='event'),
-    path('events/', EventListView.as_view(), name='event-list'),
+    path('', EventListView.as_view(), name='event-list'),
     path('<int:pk>/details', EventDetailView.as_view(), name='event-details'),
     path('add/', EventCreateView.as_view(), name='event-create'),
     path('<int:pk>/update', EventUpdateView.as_view(), name='event-update'),
