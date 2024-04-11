7f986bdc804dddb607f35ea16be7ff63897064e5
Ciella <daniella.francisco@student.ateneo.edu>
2024-04-12 05:40:48 +0800

    Enabled conversion of uploaded image extension to webp for optimization in loading the images on the web app

--- redbrickboard/event_management/models.py
+++ redbrickboard/event_management/models.py
@@ -1,16 +1,16 @@
 from django.db import models
+from django_resized import ResizedImageField
 from django.utils import timezone
 from django.urls import reverse
-from datetime import timedelta
 from accounts import models as accounts
+# from datetime import timedelta
 
 class Event(models.Model):
     event_name = models.CharField(default='', max_length=150)
     event_datetime_start = models.DateTimeField(default=timezone.now, null=False)
     event_datetime_end = models.DateTimeField(default=None, null=True)
     event_organizer = models.ForeignKey(accounts.CustomUser, on_delete=models.CASCADE, related_name='events_organized')
-    # finalize arguments
-    event_header = models.ImageField(upload_to='headers/', height_field=None, width_field=None, max_length=100, blank=True)
+    event_header = ResizedImageField(size=[815, 315], crop=['middle', 'center'], quality=75, force_format='WebP', upload_to=rename_image('headers/'))
     last_time_bumped = models.DateTimeField()
 
     def __str__(self):
