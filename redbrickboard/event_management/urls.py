cbacbdd6f3a31900779e872b96df598434fbbfa5
Ciella <daniella.francisco@student.ateneo.edu>
2024-04-13 19:54:20 +0800

    Edited URLs to enable deletion of images for Promo

--- redbrickboard/event_management/urls.py
+++ redbrickboard/event_management/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import EventListView, EventDetailView, EventCreateView, EventUpdateView
+from .views import EventListView, EventDetailView, EventCreateView, EventUpdateView, delete_image
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event-list'),
     path('<int:pk>/details', EventDetailView.as_view(), name='event-details'),
     path('add/', EventCreateView.as_view(), name='event-create'),
     path('<int:pk>/update', EventUpdateView.as_view(), name='event-update'),
+    path('int<pk>/delete-image', delete_image, name='delete_image'),
 ]
 
 app_name = "event_management"
