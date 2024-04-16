70e5c5a756981aed4abcfcdb0697494ed778c414
gab <gabriel.geraldo@student.ateneo.edu>
2024-04-16 19:19:36 +0800

    created Attendance model for registration and attendance

--- redbrickboard/event_management/admin.py
+++ redbrickboard/event_management/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Event
+from .models import Event, Attendance
 
 admin.site.register(Event)
+admin.site.register(Attendance)
